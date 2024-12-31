17961f5821f2d78b6bfab9e53173f7e9a506fb43
BigButterGuy <73145760+BigButterGuy@users.noreply.github.com>
2024-12-30 21:45:48 -0500

    Fixed controller and fxml setup, now properly loading javaFx build.

--- src/main/java/design/voight/Main.java
+++ src/main/java/design/voight/Main.java
@@ -1,8 +1,22 @@
 package design.voight;
 
-public class Main {
-    public static void main(String[] args) {
-        ProjectManager manager = new ProjectManager();
-        manager.addProject();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/interface.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("hello World");
+        stage.setScene(scene);
+        stage.show();
+        /*ProjectManager manager = new ProjectManager();
+        manager.addProject();*/
     }
+    public static void main(String[] args) {launch();}
 }
--- src/main/java/design/voight/MainController.java
+++ src/main/java/design/voight/MainController.java
@@ -1,7 +1,14 @@
 package design.voight;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainController {
+    @FXML
+    private Label welcomeText;
 
-
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
